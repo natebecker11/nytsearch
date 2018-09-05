56902db7164fbd78b3fea0b0e55307e16c9a2eb7
natebecker11 <natebecker11@gmail.com>
2018-09-04 20:57:07 -0400

    basic search complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
 var apiKey = 'bd3af344ec534cf297eb0fd6922d272e';
+var queryURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bd3af344ec534cf297eb0fd6922d272e'
+
+
+
+var getArticles = function(term, number, startYear, endYear) {
+  $.get({
+    url: queryURL + '&q=' + term
+  })
+    .then(function(response) {
+      console.log(response);
+      var articleArray = response.response.docs;
+      for (i = 0; i < number; i++) {
+        var thisArticle = articleArray[i];
+        var headline = thisArticle.headline.main;
+        var articleLink = thisArticle.web_url;
+        console.log(headline + articleLink);
+        var li = $('<li>');
+        li.addClass('list-group-item')        
+        var a = $('<a>');
+        $(a).text(headline).attr('href', articleLink).appendTo($(li));
+        $(li).appendTo('#articleBody');
+      }
+    })
+}
+
+
+$(document).on('click', '#searchBtn', function() {
+  var number = $('#numberRecordsRet').val();
+  var term = $('#searchTermIn').val().trim();
+  if (!term) return;
+  else {
+    $('#articleBody').empty();
+    getArticles(term, number);
+  }
+})
